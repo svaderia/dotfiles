c289e294e50485648f49617868ef17fc96b47eb9
svaderia <vaderiashyamal@gmail.com>
2024-02-28 08:20:32 -0500

    GPG_TTY export changed
    Read below links to see why above not always work. I faced this with a remote server today so changing this.
    https://stackoverflow.com/questions/41052538/git-error-gpg-failed-to-sign-data
    https://unix.stackexchange.com/questions/608842/zshrc-export-gpg-tty-tty-says-not-a-tty

--- .config/ohmyzsh/profile.zsh
+++ .config/ohmyzsh/profile.zsh
@@ -1,5 +1,11 @@
 export LC_ALL=en_US.UTF-8
-export GPG_TTY=$(tty)
+
+# export GPG_TTY=$(tty)
+# Read below links to see why above not always work. I faced this with a remote server today so changing this.
+# https://stackoverflow.com/questions/41052538/git-error-gpg-failed-to-sign-data
+# https://unix.stackexchange.com/questions/608842/zshrc-export-gpg-tty-tty-says-not-a-tty
+export GPG_TTY=$TTY
+
 export HISTSIZE=100000
 export PATH=$PATH:$HOME/.poetry/bin
 export CCTEMPLATEPATH=$HOME/Shyamal/GitHub/Competitive-Coding/TEMPLATE
